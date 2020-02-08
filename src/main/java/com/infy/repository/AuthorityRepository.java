301577af657d6151dd7dbb90b4255f9b109729ca
Hitesh C <jainhitesh9998@gmail.com>
2020-02-08 19:44:45 +0530

    Initial version of openhack generated by JHipster-6.3.0

---
+++ src/main/java/com/infy/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.infy.repository;
+
+import com.infy.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/infy/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,9 @@
+package com.infy.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
